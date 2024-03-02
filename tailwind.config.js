3b7b6c200490f31705ff4d04f63452932ae63d98
allan <allanrey.macasaquit@gmail.com>
2024-03-02 15:02:23 -0500

    set up and create login, register components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,33 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const generateSizeClass = (upToSize, startAt = 80) => {
-	const classes = {};
-	for (let i = startAt; i < upToSize / 4; i += 4) {
-		classes[i] = `${(i * 4) / 16}rem`;
-	}
+  const classes = {};
+  for (let i = startAt; i < upToSize / 4; i += 4) {
+    classes[i] = `${(i * 4) / 16}rem`;
+  }
 
-	return classes;
+  return classes;
 };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		screens: {
-			xxs: '300px',
-			xs: '475px',
-			...defaultTheme.screens
-		},
-		extend: {
-			width: generateSizeClass(1024),
-			minHeight: generateSizeClass(1024, 0),
-			maxHeight: generateSizeClass(1024, 0),
-			maxWidth: generateSizeClass(1024, 0),
-			minWidth: generateSizeClass(1024, 0),
-			borderWidth: {
-				1: '1px'
-			}
-		}
-	}
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    screens: {
+      xxs: "300px",
+      xs: "475px",
+      ...defaultTheme.screens
+    },
+    extend: {
+      width: generateSizeClass(1024),
+      minHeight: generateSizeClass(1024, 0),
+      maxHeight: generateSizeClass(1024, 0),
+      maxWidth: generateSizeClass(1024, 0),
+      minWidth: generateSizeClass(1024, 0),
+      borderWidth: {
+        1: "1px"
+      }
+    }
+  },
+  plugins: [require("@tailwindcss/forms")]
 };
