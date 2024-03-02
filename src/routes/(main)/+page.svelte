b837430c0f3783d05ec207708706935eae32c1d1
allan <allanrey.macasaquit@gmail.com>
2024-03-02 15:41:43 -0500

    fix intellisense

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,6 +1,6 @@
 <script>
+  import GlidePost from "$components/glides/GlidePost.svelte";
   import TiImageOutline from "svelte-icons/ti/TiImageOutline.svelte";
-  import GlidePost from "../../components/glides/GlidePost.svelte";
   let glides = [];
   let glideContent = "";
 
