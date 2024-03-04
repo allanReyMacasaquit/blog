b44d0ed8c98db2764e74476a1228adbecd680329
allan <allanrey.macasaquit@gmail.com>
2024-03-03 21:56:06 -0500

    loading simulation

--- src/components/context/auth/AuthContext.svelte
+++ src/components/context/auth/AuthContext.svelte
@@ -2,15 +2,20 @@
   import { setContext } from "svelte";
   import { writable } from "svelte/store";
   import { key } from ".";
+  import Loader from "$utils/Loader.svelte";
 
-  let isAuthenticated = writable(false);
-  let loading = writable(false);
+  let isLoading = writable(true);
+  let isAuthenticated = writable(false, (set) => {
+    setTimeout(() => {
+      set(true); // isAuthenticated setter
+      isLoading.set(false);
+    }, 2000);
+  });
 
-  setContext(key),
-    {
-      isAuthenticated,
-      loading
-    };
+  setContext(key, {
+    isAuthenticated,
+    isLoading
+  });
 </script>
 
-<slot />
+<Loader size={30} />
--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,14 +1,17 @@
 <script>
   import { getUIContext } from "$components/context/UI";
+  import { getAuthContext } from "$components/context/auth";
 
   import GlidePost from "$components/glides/GlidePost.svelte";
+  import TiBrush from "svelte-icons/ti/TiBrush.svelte";
 
   import TiImageOutline from "svelte-icons/ti/TiImageOutline.svelte";
 
   let glides = [];
   let glideContent = "";
 
-  const { isXl, isLg } = getUIContext();
+  const { isXl } = getUIContext();
+  const { isAuthenticated, isLoading } = getAuthContext();
 
   function createGlide() {
     let date = new Date();
@@ -48,13 +51,13 @@
       <textarea
         bind:value={glideContent}
         name="content"
-        rows="1"
+        rows="2"
         id="glide"
         class="bg-transparent resize-none overflow-hidden block !outline-none !border-none border-transparent focus:border-transparent focus:ring-0 text-gray-100 text-xl w-full p-0"
         placeholder={"What's new?"}
       />
     </div>
-    <div class="flex-it mb-1 flex-row xs:justify-between items-center">
+    <div class="flex-it mb-1 flex-row justify-end items-center">
       <div class="flex-it mt-3 mr-3 cursor-pointer text-white hover:text-blue-400 transition">
         <div class="upload-btn-wrapper">
           <div class="icon">
@@ -63,14 +66,18 @@
           <input type="file" name="myfile" />
         </div>
       </div>
-      <div class="flex-it w-32 mt-3 cursor-pointer">
+      <div class="flex-it w-12 sm:w-24 mt-3 cursor-pointer">
         <button
           on:click={createGlide}
           type="button"
           class="disabled:cursor-not-allowed disabled:bg-gray-400 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
         >
           <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
-            <span>Glide It</span>
+            {#if $isXl}
+              <span>Glide It</span>
+            {:else}
+              <div class="icon"><TiBrush /></div>
+            {/if}
           </div>
         </button>
       </div>
@@ -79,6 +86,8 @@
   <!-- MESSENGER END -->
 </div>
 <div class="h-px bg-gray-700 my-1" />
+Is Auth: {JSON.stringify($isAuthenticated)}
+Is loading: {JSON.stringify($isLoading)}
 <!-- GLIDE POST START -->
 {#each glides as glide (glide.id)}
   <GlidePost {glide} />
