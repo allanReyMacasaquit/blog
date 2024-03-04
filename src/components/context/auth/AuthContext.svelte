50b0019d80f9b231b9738414ef4a410d1df625ad
allan <allanrey.macasaquit@gmail.com>
2024-03-03 21:01:36 -0500

    get and set auth context

---
+++ src/components/context/auth/AuthContext.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { setContext } from "svelte";
+  import { writable } from "svelte/store";
+  import { key } from ".";
+
+  let isAuthenticated = writable(false);
+  let loading = writable(false);
+
+  setContext(key),
+    {
+      isAuthenticated,
+      loading
+    };
+</script>
+
+<slot />
